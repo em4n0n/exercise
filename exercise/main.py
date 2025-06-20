aceb208f2f72482821d04168ddd148ba5d5cb93d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-06-20 15:52:07 -0700

    Update main.py

--- exercise/main.py
+++ exercise/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 @app.route('/generate-plan', methods=['POST'])
 def generate_plan():
