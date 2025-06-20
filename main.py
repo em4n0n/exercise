09c70736309864cb95298a3ab91fa54c6ed95493
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-06-20 13:14:10 -0700

    Update template refernce

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return open("index.html").read()
+    return open("templates/index.html").read()
 
 @app.route('/generate-plan', methods=['POST'])
 def generate_plan():
